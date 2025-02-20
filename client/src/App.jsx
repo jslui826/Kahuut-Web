c2fdaad1ddf85dd78484c88bcff68a312614c010
jslui826 <jslui826@gmail.com>
2025-02-19 21:20:25 -0800

    Restored old functionalities to branch

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,23 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Route, Routes, useNavigate } from 'react-router-dom'
 import './App.css'
+import Start from './pages/start'
+import Login from './pages/login'
+import Register from './pages/register'
+import Home from './pages/home'
 import Navbar from './components/navbar'
 import Buttons from './components/buttons'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <Navbar></Navbar>
-      <Buttons></Buttons>
-    </>
-  )
-}
+const App = () => (
+  <div style={{ textAlign: "center" }}>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/start" element={<Start />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
+  </div>
+)
 
 export default App
---
+++ client/src/pages/home.jsx
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import '../css/home.css'
+
+const Home = () => {
+    const title = "Kahuut".split("");
+    const [animationComplete, setAnimationComplete] = useState(false);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        setTimeout(() => {
+            setAnimationComplete(true);
+        }, 4000);
+    }, []);
+
+    return (
+        <div className="loader-container">
+            {/* Floating Animated Bubbles */}
+            {[...Array(15)].map((_, index) => (
+                <div key={index} className="bubble">
+                    <div className="dot"></div>
+                </div>
+            ))}
+
+            {/* Kahuut Logo Animation */}
+            <div className="flipping-cards">
+                {title.map((letter, index) => (
+                    <div
+                        key={index}
+                        className={`card ${animationComplete ? "jump" : "flip-in"} card-${index}`}
+                    >
+                        {letter}
+                    </div>
+                ))}
+            </div>
+
+            {/* Start Button - Navigates to Start Page */}
+            <button className="press-button" onClick={() => navigate("/start")}>
+                Start
+            </button>
+        </div>
+    );
+};
+
+export default Home;
