b8a745d3e8ea0f2b38b1b3e1d4abba79fbc34e46
jslui826 <jslui826@gmail.com>
2025-03-08 14:13:54 -0800

    Added fetch request in register

--- client/src/pages/register.jsx
+++ client/src/pages/register.jsx
@@ -1,28 +1,61 @@
-const Register = () => (
-    <div class="relative flex flex-col items-center justify-center h-screen overflow-hidden">
-        <div class="w-full p-6 bg-white border-t-4 border-gray-600 rounded-md shadow-md border-top lg:max-w-lg">
-            <h1 class="text-3xl font-semibold text-center text-gray-700">
-                Register
-            </h1>
-            <form class="space-y-4">
-                <div>
-                    <label class="label">
-                        <span class="text-base label-text">Email</span>
-                    </label>
-                    <input type="text" placeholder="Email Address" class="w-full input input-bordered" />
-                </div>
-                <div>
-                    <label class="label">
-                        <span class="text-base label-text">Password</span>
-                    </label>
-                    <input type="password" placeholder="Enter Password" class="w-full input input-bordered" />
-                </div>
-                <div>
-                    <button class="btn btn-block btn-neutral">Sign Up</button>
-                </div>
-            </form>
+import PropTypes from 'prop-types'
+import { useState } from 'react'
+
+async function signUpUser(credentials) {
+    return fetch('http://localhost:8080/signup', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(credentials)
+    })
+        .then(data => data.json())
+}
+
+function Register() {
+    const [username, setUserName] = useState();
+    const [password, setPassword] = useState();
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        const token = await signUpUser({
+            username,
+            password
+        });
+        setToken(token);
+    }
+
+    return(
+        <div class="relative flex flex-col items-center justify-center h-screen overflow-hidden">
+            <div class="w-full p-6 bg-white border-t-4 border-gray-600 rounded-md shadow-md border-top lg:max-w-lg">
+                <h1 class="text-3xl font-semibold text-center text-gray-700">
+                    Register
+                </h1>
+                <form class="space-y-4" onSubmit={handleSubmit}>
+                    <div>
+                        <label class="label">
+                            <span class="text-base label-text">Email</span>
+                        </label>
+                        <input type="text" placeholder="Email Address" class="w-full input input-bordered" onChange={e => setUserName(e.target.value)} />
+                    </div>
+                    <div>
+                        <label class="label">
+                            <span class="text-base label-text">Password</span>
+                        </label>
+                        <input type="password" placeholder="Enter Password" class="w-full input input-bordered" onChange={e => setPassword(e.target.value)} />
+                    </div>
+                    <a href="../login" class="text-xs text-gray-600 hover:underline hover:text-blue-600">Already have an account?</a>
+                    <div>
+                        <button class="btn btn-block btn-neutral">Sign Up</button>
+                    </div>
+                </form>
+            </div>
         </div>
-    </div>
-)
+    )
+}
 
 export default Register
+
+Register.propTypes = {
+    setToken: PropTypes.func.isRequired
+}
