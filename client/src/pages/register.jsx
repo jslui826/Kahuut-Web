daaaa61e630f28b6026341d8672e2fce07997879
jslui826 <jslui826@gmail.com>
2025-03-11 19:30:48 -0700

    Replaced instances of 'class' with 'className'

--- client/src/pages/register.jsx
+++ client/src/pages/register.jsx
@@ -30,28 +30,28 @@ async function registerUser(credentials) {
     }
 
     return(
-        <div class="relative flex flex-col items-center justify-center h-screen overflow-hidden">
-            <div class="w-full p-6 bg-white border-t-4 border-gray-600 rounded-md shadow-md border-top lg:max-w-lg">
-                <h1 class="text-3xl font-semibold text-center text-gray-700">
+        <div className="relative flex flex-col items-center justify-center h-screen overflow-hidden">
+            <div className="w-full p-6 bg-white border-t-4 border-gray-600 rounded-md shadow-md border-top lg:max-w-lg">
+                <h1 className="text-3xl font-semibold text-center text-gray-700">
                     Register
                 </h1>
                 
-                <form class="space-y-4" onSubmit={handleSubmit}>
+                <form className="space-y-4" onSubmit={handleSubmit}>
                     <div>
-                        <label class="label">
-                            <span class="text-base label-text">Email</span>
+                        <label className="label">
+                            <span className="text-base label-text">Email</span>
                         </label>
-                        <input type="email" placeholder="Email Address" class="w-full input input-bordered" onChange={e => setEmail(e.target.value)} required/>
+                        <input type="email" placeholder="Email Address" className="w-full input input-bordered" onChange={e => setEmail(e.target.value)} required/>
                     </div>
                     <div>
-                        <label class="label">
-                            <span class="text-base label-text">Password</span>
+                        <label className="label">
+                            <span className="text-base label-text">Password</span>
                         </label>
-                        <input type="password" placeholder="Enter Password" class="w-full input input-bordered" onChange={e => setPassword(e.target.value)} />
+                        <input type="password" placeholder="Enter Password" className="w-full input input-bordered" onChange={e => setPassword(e.target.value)} />
                     </div>
-                    <a href="../login" class="text-xs text-gray-600 hover:underline hover:text-blue-600">Already have an account?</a>
+                    <a href="../login" className="text-xs text-gray-600 hover:underline hover:text-blue-600">Already have an account?</a>
                     <div>
-                        <button class="btn btn-block btn-neutral">Sign Up</button>
+                        <button className="btn btn-block btn-neutral">Sign Up</button>
                     </div>
                 </form>
             </div>
