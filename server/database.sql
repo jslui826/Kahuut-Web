c98af7bbfc5d38d0eb0d730eacfae9cbf4694c62
Kyle Zheng <zhengkyle811@gmail.com>
2025-03-11 23:33:22 -0700

    Changed database schema AGAIN

--- server/database.sql
+++ server/database.sql
@@ -6,27 +6,37 @@ CREATE TABLE persons (
 );
 
 CREATE TABLE quizzes (
-    quiz_id INT NOT NULL,
+    quiz_id SERIAL PRIMARY KEY,
     title TEXT NOT NULL,
     creator_id INT NOT NULL REFERENCES persons(person_id) ON DELETE CASCADE,
+    audio BYTEA,
+    image BYTEA
+);
+
+CREATE TABLE qa (
+    quiz_id INT NOT NULL REFERENCES quizzes(quiz_id) ON DELETE CASCADE,
     question TEXT NOT NULL,
-    answer1 TEXT NOT NULL,
+    answer1 TEXT NOT NULL, -- Assume answer1 is always correct
     answer2 TEXT NOT NULL,
     answer3 TEXT NOT NULL,
     answer4 TEXT NOT NULL,
-    audio BYTEA,
-    image BYTEA,
     PRIMARY KEY (quiz_id, question)
 );
 
-CREATE TABLE profiles (
-    profile_id SERIAL PRIMARY KEY,
+CREATE TABLE favorites (
     person_id INT NOT NULL REFERENCES persons(person_id) ON DELETE CASCADE,
-    quiz_id INT NOT NULL,
-    question TEXT NOT NULL,
-    FOREIGN KEY (quiz_id, question) REFERENCES quizzes(quiz_id, question) ON DELETE CASCADE,
-    UNIQUE(person_id, quiz_id, question) -- Ensures each person has a unique profile entry per quiz question
+    quiz_id INT NOT NULL REFERENCES quizzes(quiz_id) ON DELETE CASCADE,
+    PRIMARY KEY (person_id, quiz_id)
 );
+
+CREATE TYPE team_enum AS ENUM ('R', 'Y', 'B');
+
+CREATE TABLE profile (
+    person_id INT PRIMARY KEY REFERENCES persons(person_id) ON DELETE CASCADE,
+    team team_enum NOT NULL,
+    score INT DEFAULT 0,
+    pfp BYTEA
+)
 /*
 This is just a place to put the commands we will put into a sql session. It is not supposed to be run.
 We assume answer 1 is the TRUE answer.
