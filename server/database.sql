7105737e16dcb5c0851dd184f45aa4cbdf62ab6d
Kyle Zheng <zhengkyle811@gmail.com>
2025-03-11 22:49:08 -0700

    Change database schema to make sense for multiple questions per quiz

--- server/database.sql
+++ server/database.sql
@@ -1,4 +1,4 @@
- CREATE TABLE persons (
+CREATE TABLE persons (
     person_id SERIAL PRIMARY KEY,
     email VARCHAR(255) UNIQUE NOT NULL,
     password TEXT NOT NULL,
@@ -6,7 +6,7 @@
 );
 
 CREATE TABLE quizzes (
-    quiz_id SERIAL PRIMARY KEY,
+    quiz_id INT NOT NULL,
     title TEXT NOT NULL,
     creator_id INT NOT NULL REFERENCES persons(person_id) ON DELETE CASCADE,
     question TEXT NOT NULL,
@@ -15,18 +15,22 @@ CREATE TABLE quizzes (
     answer3 TEXT NOT NULL,
     answer4 TEXT NOT NULL,
     audio BYTEA,
-    image1 BYTEA,
-    image2 BYTEA,
-    image3 BYTEA,
-    image4 BYTEA
+    image BYTEA,
+    PRIMARY KEY (quiz_id, question)
 );
 
 CREATE TABLE profiles (
     profile_id SERIAL PRIMARY KEY,
     person_id INT NOT NULL REFERENCES persons(person_id) ON DELETE CASCADE,
-    quiz_id INT NOT NULL REFERENCES quizzes(quiz_id) ON DELETE CASCADE,
-    UNIQUE(person_id, quiz_id)  -- Ensures each person can have a unique profile per quiz
+    quiz_id INT NOT NULL,
+    question TEXT NOT NULL,
+    FOREIGN KEY (quiz_id, question) REFERENCES quizzes(quiz_id, question) ON DELETE CASCADE,
+    UNIQUE(person_id, quiz_id, question) -- Ensures each person has a unique profile entry per quiz question
 );
 /*
 This is just a place to put the commands we will put into a sql session. It is not supposed to be run.
+We assume answer 1 is the TRUE answer.
+To reset the table, we use the following: 
+DROP SCHEMA public CASCADE;
+CREATE SCHEMA public;
 */
